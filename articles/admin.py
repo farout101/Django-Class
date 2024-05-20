d912367f70b27a6921d302749c61165505b4aa29
Phyo Zaw Linn <phyozawlinn1852020@gmail.com>
2024-05-20 23:47:20 +0630

    blank submit

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Article
+
+#custom admin view
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'content']
+    search_fields = ['id', 'title']
+
+admin.site.register(Article, ArticleAdmin) # can adjust the admin view 
